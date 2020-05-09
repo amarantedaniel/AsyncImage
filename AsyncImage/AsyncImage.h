32c902b4519fe75b8be886627ec8da8f90766c25
Daniel Amarante <daniel.amarante2@gmail.com>
2020-05-09 17:51:07 -0300

    Initial Commit

---
+++ AsyncImage/AsyncImage.h
@@ -0,0 +1,19 @@
+//
+//  AsyncImage.h
+//  AsyncImage
+//
+//  Created by Daniel Amarante on 5/9/20.
+//  Copyright © 2020 Tigerhood. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AsyncImage.
+FOUNDATION_EXPORT double AsyncImageVersionNumber;
+
+//! Project version string for AsyncImage.
+FOUNDATION_EXPORT const unsigned char AsyncImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsyncImage/PublicHeader.h>
+
+
