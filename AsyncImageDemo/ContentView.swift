3ea6b014a1fe0cb2e7185ba0bb92aafa08a6041c
Daniel Amarante <daniel.amarante2@gmail.com>
2020-05-09 17:54:53 -0300

    Initial project setup

---
+++ AsyncImageDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AsyncImageDemo
+//
+//  Created by Daniel Amarante on 5/9/20.
+//  Copyright © 2020 Tigerhood. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
